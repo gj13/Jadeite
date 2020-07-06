c52727f0e62c30b0ed9f01b3967f2a80d0f64f74
George <gj13.forward@gmail.com>
2020-07-06 18:50:02 -0400

    Corrected old reference to DSP effect.

--- scripts/globals/items/rabbit_pie.lua
+++ scripts/globals/items/rabbit_pie.lua
@@ -23,7 +23,7 @@ function onItemCheck(target)
 end
 
 function onItemUse(target)
-    target:addStatusEffect(dsp.effect.FOOD,0,0,10800,5685) -- changed duration from 1800
+    target:addStatusEffect(tpz.effect.FOOD,0,0,10800,5685) -- changed duration from 1800
 end
 
 function onEffectGain(target,effect)
